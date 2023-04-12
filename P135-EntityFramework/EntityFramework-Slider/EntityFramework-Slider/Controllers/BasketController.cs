77df6ca828a779cb39572390bd1c0c324744f44d
unknown <jahandarmv@code.edu.az>
2023-04-12 21:43:32 +0400

    new

---
+++ P135-EntityFramework/EntityFramework-Slider/EntityFramework-Slider/Controllers/BasketController.cs
@@ -0,0 +1,41 @@
+﻿using EntityFramework_Slider.Data;
+using EntityFramework_Slider.Models;
+using EntityFramework_Slider.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+namespace EntityFramework_Slider.Controllers
+{
+    public class BasketController : Controller
+    {
+        private readonly AppDbContext _context;
+        public BasketController(AppDbContext context)
+        {
+        
+            _context = context;
+
+        }
+
+
+        public async Task <IActionResult>Index()
+        {
+            List<BasketVM> basket;
+            if (Request.Cookies["basket"] != null)
+            {
+                basket = JsonConvert.DeserializeObject<List<BasketVM>>(Request.Cookies["basket"]);
+            }
+            else
+            {
+                basket = new List<BasketVM>();
+            }
+
+            foreach (var basketVM in basket)
+            {
+                Product dbProduct =  _context.Products.Include(m => m.Images).FirstOrDefault(m => m.Id == basketVM.Id);
+                basketVM.Product = dbProduct;
+            }
+            return View(basket);
+        }
+    }
+}
---
+++ P135-EntityFramework/EntityFramework-Slider/EntityFramework-Slider/ViewModels/BasketVM.cs
@@ -0,0 +1,11 @@
+﻿using EntityFramework_Slider.Models;
+
+namespace EntityFramework_Slider.ViewModels
+{
+    public class BasketVM
+    {
+        public Product Product { get; set; }
+        public int Id { get; set; }
+        public int Count { get; set; }
+    }
+}
